84847b6dbdda0a19f973474b87de338bf5214d6a
Jelle Be-Value <be-value@live.nl>
2020-01-08 09:41:53 +0100

    1. Client Credentials MVC application accessing authorized resource with access token retrieved from the secure token service (STS) - aka AuthorizationService - using client credentials (client-id/client-secret) shared between the client and the STS

---
+++ AuthorizationService/Config.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using IdentityServer4.Models;
+
+namespace AuthorizationService
+{
+    public class Config
+    {
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("resourceApi", "API Application")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "clientApp",
+
+                    // no interactive user, use the clientid/secret for authentication
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                    // secret for authentication
+                    ClientSecrets =
+                    {
+                        new Secret("very secret".Sha256())
+                    },
+
+                    AllowedScopes = { "resourceApi" }
+                }
+            };
+        }
+    }
+}
---
+++ ClientApp/Controllers/IdentityController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using IdentityModel.Client;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace ClientApp.Controllers
+{
+    [Route("[controller]")]
+    public class IdentityController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var client = new HttpClient();
+
+            // discover endpoints from metadata
+            var discoveryClient = client;
+            var oidcDiscoveryResult = await discoveryClient.GetDiscoveryDocumentAsync("http://localhost:5000");
+            if (oidcDiscoveryResult.IsError)
+            {
+                Console.WriteLine(oidcDiscoveryResult.Error);
+                return Json(oidcDiscoveryResult.Error);
+            }
+
+            // request token from token endpoint
+            var tokenClient = client;
+            var tokenResponse = await tokenClient.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+            {
+                Address = oidcDiscoveryResult.TokenEndpoint,
+
+                ClientId = "clientApp",
+                ClientSecret = "very secret",
+
+                Parameters =
+                {
+                    { "scope", "resourceApi" }
+                }
+            });
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                throw new HttpRequestException(tokenResponse.Error);
+            }
+            Console.WriteLine(tokenResponse.Json);
+            Console.WriteLine("\n\n");
+
+            // call api with access token
+            client.SetBearerToken(tokenResponse.AccessToken);
+            var response = await client.GetAsync("http://localhost:5001/identity");
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.StatusCode);
+                throw new HttpRequestException(response.StatusCode.ToString());
+            }
+
+            var content = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(JArray.Parse(content));
+            return Json(content);
+        }
+    }
+}
